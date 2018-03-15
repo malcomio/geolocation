b95079bf5b68b15aa85bfc485e99e7fa922c1b55
ChristianAdamski <ChristianAdamski@867880.no-reply.drupal.org>
2018-03-15 17:52:18 +0100

    Issue #2953277 by ChristianAdamski: Generated sample out of range

--- src/Plugin/Field/FieldType/GeolocationItem.php
+++ src/Plugin/Field/FieldType/GeolocationItem.php
@@ -101,8 +101,8 @@ class GeolocationItem extends FieldItemBase {
    * {@inheritdoc}
    */
   public static function generateSampleValue(FieldDefinitionInterface $field_definition) {
-    $values['lat'] = rand(-90, 90) - rand(0, 999999) / 1000000;
-    $values['lng'] = rand(-180, 180) - rand(0, 999999) / 1000000;
+    $values['lat'] = rand(-89, 90) - rand(0, 999999) / 1000000;
+    $values['lng'] = rand(-179, 180) - rand(0, 999999) / 1000000;
     return $values;
   }
 
