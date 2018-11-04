79a3e824606ddac466d73fc43cc2e276f7d06587
ChristianAdamski <ChristianAdamski@867880.no-reply.drupal.org>
2018-11-04 10:33:58 +0100

    Issue #3008141 by ChristianAdamski: Add Yandex Maps provider

---
+++ modules/geolocation_yandex/js/MapFeature/control-traffic.js
@@ -0,0 +1,33 @@
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Search control.
+   *
+   * @type {Drupal~behavior}
+   *
+   * @prop {Drupal~behaviorAttach} attach
+   *   Attaches common map traffic functionality to relevant elements.
+   */
+  Drupal.behaviors.yandexControlTraffic = {
+    attach: function (context, drupalSettings) {
+      Drupal.geolocation.executeFeatureOnAllMaps(
+        'yandex_control_traffic',
+
+        /**
+         * @param {GeolocationYandexMap} map - Current map.
+         * @param {Object} featureSettings - Settings for current feature.
+         */
+        function (map, featureSettings) {
+          map.yandexMap.controls.add('trafficControl');
+
+          return true;
+        },
+        drupalSettings
+      );
+    },
+    detach: function (context, drupalSettings) {}
+  };
+
+})(jQuery, Drupal);
