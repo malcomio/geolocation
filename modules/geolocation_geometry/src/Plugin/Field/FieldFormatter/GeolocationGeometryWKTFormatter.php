d6850bccbafd0c33344bb7753da3c817480ebfc3
AndyLicht <AndyLicht@2496914.no-reply.drupal.org>
2019-12-04 17:46:31 +0100

    Issue #3098790 by AndyLicht: WKT formatter and widget bug

--- modules/geolocation_geometry/src/Plugin/Field/FieldFormatter/GeolocationGeometryWKTFormatter.php
+++ modules/geolocation_geometry/src/Plugin/Field/FieldFormatter/GeolocationGeometryWKTFormatter.php
@@ -12,7 +12,14 @@ use Drupal\Core\Field\FormatterBase;
  *   id = "geolocation_geometry_wkt",
  *   label = @Translation("Geolocation Geometry WKT"),
  *   field_types = {
- *     "geolocation_geometry"
+ *     "geolocation_geometry_geometry",
+ *     "geolocation_geometry_geometrycollection",
+ *     "geolocation_geometry_point",
+ *     "geolocation_geometry_linestring",
+ *     "geolocation_geometry_polygon",
+ *     "geolocation_geometry_multipoint",
+ *     "geolocation_geometry_multilinestring",
+ *     "geolocation_geometry_multipolygon",
  *   }
  * )
  */
