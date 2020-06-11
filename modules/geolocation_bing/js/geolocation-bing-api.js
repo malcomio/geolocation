29d05acb488f6047d4e8412d3a0539346759a32f
Malcolm Young <malcolm@rubydesign.co.uk>
2020-06-11 17:24:59 +0100

    get basic map displaying

--- modules/geolocation_bing/js/geolocation-bing-api.js
+++ modules/geolocation_bing/js/geolocation-bing-api.js
@@ -21,39 +21,8 @@
    */
   function GeolocationBingMap(mapSettings) {
 
-    console.log(mapSettings.settings.bing_settings);
-    var bingPromise = new Promise(function (resolve, reject) {
-
-
-
-      const mm = Microsoft.Maps,
-        center = new mm.Location(latitude, longitude),
-        pinLayer = new Microsoft.Maps.EntityCollection();
-      const pin = new Microsoft.Maps.Pushpin(center, {
-        icon: drupalSettings.pin,
-      });
-
-      let map = new mm.Map('.rml-branch-finder-map', {
-        credentials: mapSettings.settings.bing_settings.api_key,
-        center: center,
-        showDashboard: false,
-        showScalebar: true,
-        disableZooming: true,
-        disablePanning: true,
-        allowHidingLabelsOfRoad: false,
-        showLocateMeButton: false,
-        showCopyright: false,
-        showLogo: false,
-        zoom: 15,
-      });
-
-      pinLayer.push(pin);
-      map.entities.push(pinLayer);
-
-
-      console.log(map);
-
-      // TODO: get rid
+    const bingPromise = new Promise(function (resolve, reject) {
+      console.log('in promise');
       if (typeof Microsoft === 'undefined') {
         setTimeout(function () {
           if (typeof Microsoft === 'undefined') {
@@ -90,34 +59,56 @@
       width: this.settings.bing_settings.width
     });
 
-    var that = this;
-
+    let that = this;
+    // Ensure that the Bing script has loaded before trying to do anything.
     bingPromise.then(function () {
 
+      that.addInitializedCallback(function (map) {
+
+        const bingSettings = map.settings.bing_settings;
+
+        map.bingMap = new Microsoft.Maps.Map(map.container[0], {
+          credentials: bingSettings.api_key,
+        });
+
+        const mapCenter = new Microsoft.Maps.Location(map.lat, map.lng);
+        map.bingMap.setOptions({
+          // center: mapCenter,
+          showDashboard: false,
+          showScalebar: true,
+          disableZooming: true,
+          disablePanning: true,
+          allowHidingLabelsOfRoad: false,
+          showLocateMeButton: false,
+          showCopyright: false,
+          showLogo: false,
+          zoom: bingSettings.zoom,
+        });
+
+        console.log(map);
+      });
+
       that.addPopulatedCallback(function (map) {
-        var singleClick;
-        map.bingMap.on('click', /** @param {BingMouseEvent} e */ function (e) {
-          singleClick = setTimeout(function () {
-            map.clickCallback({lat: e.latlng.lat, lng: e.latlng.lng});
-          }, 500);
-        });
-
-        map.bingMap.on('dblclick', /** @param {BingMouseEvent} e */ function (e) {
-          clearTimeout(singleClick);
-          map.doubleClickCallback({lat: e.latlng.lat, lng: e.latlng.lng});
-        });
-
-        map.bingMap.on('contextmenu', /** @param {BingMouseEvent} e */ function (e) {
-          map.contextClickCallback({lat: e.latlng.lat, lng: e.latlng.lng});
-        });
+        // var singleClick;
+        // map.bingMap.on('click', /** @param {BingMouseEvent} e */ function (e) {
+        //   singleClick = setTimeout(function () {
+        //     map.clickCallback({lat: e.latlng.lat, lng: e.latlng.lng});
+        //   }, 500);
+        // });
+        //
+        // map.bingMap.on('dblclick', /** @param {BingMouseEvent} e */ function (e) {
+        //   clearTimeout(singleClick);
+        //   map.doubleClickCallback({lat: e.latlng.lat, lng: e.latlng.lng});
+        // });
+        //
+        // map.bingMap.on('contextmenu', /** @param {BingMouseEvent} e */ function (e) {
+        //   map.contextClickCallback({lat: e.latlng.lat, lng: e.latlng.lng});
+        // });
       });
 
       that.initializedCallback();
       that.populatedCallback();
-    })
-    .catch(function (error) {
-      console.error('Bing library not loaded. Bailing out. Error:'); // eslint-disable-line no-console.
-      console.error(error);
+
     });
   }
 
